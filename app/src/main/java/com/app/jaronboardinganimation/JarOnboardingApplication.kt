b39aa46aec4a42ecc1933f8ea8f6cc881f22b3bb
Kundan <Kumar.kundan@Jeevansathi.com>
2025-08-25 15:47:38 +0530

    -- initial setup with dependencies

---
+++ app/src/main/java/com/app/jaronboardinganimation/JarOnboardingApplication.kt
@@ -0,0 +1,7 @@
+package com.app.jaronboardinganimation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JarOnboardingApplication : Application()
